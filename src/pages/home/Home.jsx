f99db9099b4bcfd2949aa409c3a0e4c917d0dd75
anhpham <hoctap31111@gmail.com>
2023-05-30 23:53:25 +0700

    fp

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,23 @@
 import "./home.css";
 import Navbar from "./../components/navbar/Navbar";
 import Header from "./../components/header/Header";
+import Futered from "./../components/featured/Fetured";
+import PropertyList from "../components/propertyList/PropertyList";
+import FeaturedProperties from "../components/featuredProperties/FeaturedProperties";
 
 function Home() {
   return (
     <div>
       <Navbar />
       <Header />
+
+      <div className="homeContainer">
+        <Futered />
+        <h1 className="homeTitle">Browse by property type</h1>
+        <PropertyList />
+        <h1 className="homeTitle">Homes guests love</h1>
+        <FeaturedProperties />
+      </div>
     </div>
   );
 }
--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,5 +1,13 @@
+import Navbar from "./../components/navbar/Navbar";
+import Header from "./../components/header/Header";
+
 function List() {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar />
+      <Header type="list" />
+    </div>
+  );
 }
 
 export default List;
